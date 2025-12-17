f7c7fa7b44176b366f69141a62355c35466338ae
parasol-ka <ananastya.chau@gmail.com>
2025-12-17 11:56:46 +0100

    Feat: add instant refresh of ec2 on change

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -53,4 +53,14 @@ resource "aws_autoscaling_group" "web" {
 
   health_check_type         = "ELB"
   health_check_grace_period = 60
+
+  instance_refresh {
+    strategy = "Rolling"
+    triggers = ["launch_template"]
+
+    preferences {
+      instance_warmup        = 60
+      min_healthy_percentage = 50
+    }
+  }
 }
